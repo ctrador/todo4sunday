ab1a6c89502de14f850b1c4c986e8b59627096db
ctrador <chad.trador@interapthq.com>
2016-10-30 11:52:25 -0400

    Add files via upload

--- toDoList4/toDoList/ToDoList/ToDoList/ToDoTableViewController.swift
+++ toDoList4/toDoList/ToDoList/ToDoList/ToDoTableViewController.swift
@@ -14,7 +14,7 @@ class ToDoTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     override func didReceiveMemoryWarning() {
